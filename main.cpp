6893fd1a8b2bcd69f56ccec740cbee63d5ee76ca
Braeden <singleton1@kenyon.edu>
2024-11-20 13:37:56 -0500

    wrote functions for sequential and binary searches

--- main.cpp
+++ main.cpp
@@ -8,17 +8,41 @@ using namespace std;
 
 // Sequential search
 int seq_search(vector<int> &keys, int key, int &numCmp) {
-  
+    return seq_search(keys,key,0,keys.size(),numCmp);
 }
 
 // Sequential search with start and stop range
 int seq_search(vector<int> &keys, int key, int start, int stop, int &numCmp) {
-
+    for (int i=start;i < stop;i++){
+        numCmp++;
+        if (keys[i]==key){
+            return i;
+        }
+    }
+    return -1;
 }
 
 // Binary search
 int bin_search(vector<int> &keys, int key, int &numCmp) {
-
+    vector<int> searchvec=keys;
+    bool found=false;
+    while (!found){
+        numCmp++;
+        int middle=searchvec.size()/2;
+        int cmpVal=searchvec[middle];
+        if (cmpVal==key){
+            return middle;
+        } else {
+            vector<int> tmp;
+            numCmp++; 
+            if (cmpVal > key) {
+                copy(searchvec.begin(),searchvec.begin()+middle,tmp.begin());
+            } else {
+                copy(searchvec.begin()+middle+1,searchvec.end(),tmp.begin());
+            }
+            searchvec=tmp;
+        }
+    }
 }
 
 // Enhanced binary search: switches to sequential search if remaining range is < 15
